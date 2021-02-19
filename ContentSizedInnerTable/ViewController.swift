72f7e7c0aff2c38b4eb046980f2d708c013c2648
Fernando <hcm415@ha.org.hk>
2021-02-19 12:26:20 +0800

    Initial Commit

---
+++ ContentSizedInnerTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContentSizedInnerTable
+//
+//  Created by Fernando on 19/2/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
